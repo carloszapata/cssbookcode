ab4c18fe418d88d0aedba9c8e47f9336c7f7e9d9
Jeremy Foster <jeremy.foster@live.com>
2013-02-15 08:16:11 -0800

    many listing additions

--- cssbookcode/Code/pages/chapter3/lst0303/lst0303.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    "use strict";
-
-    WinJS.UI.Pages.define("/pages/chapter3/lst0303/lst0303.html", {
-        ready: function (element, options) {
-
-        }
-    });
-})();
--- cssbookcode/Code/pages/chapter3/lst0313/lst0313.js
+++ cssbookcode/Code/pages/chapter3/lst0313/lst0313.js
@@ -3,7 +3,7 @@
 
     WinJS.UI.Pages.define("/pages/chapter3/lst0313/lst0313.html", {
         ready: function (element, options) {
-
+            document.querySelector("#mydiv").className = "bigAndBold";
         }
     });
 })();
