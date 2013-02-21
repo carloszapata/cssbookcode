eac8e19134b0f12e60cbc2423cce058de60efa8c
Jeremy Foster <jeremy.foster@live.com>
2013-02-21 11:50:19 -0800

    fix listing 6-27 issue

--- cssbookcode/Code/pages/chapter6/lst0627/lst0627.js
+++ cssbookcode/Code/pages/chapter6/lst0627/lst0627.js
@@ -3,7 +3,7 @@
 
     WinJS.UI.Pages.define("/pages/chapter6/lst0627/lst0627.html", {
         ready: function (element, options) {
-            var d = document.querySelector(".parent");
+            var d = document.querySelector(".lst0627 .parent");
             d.style.transform = new MSCSSMatrix(d.style.transform).rotate(0, 0, -10);
         }
     });
